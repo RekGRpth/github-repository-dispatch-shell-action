bbbc1632c8599ae642924da6513af052ed2291c3
RekGRpth <rekgrpth@gmail.com>
2021-08-05 09:55:53 +0500

    test

--- action.sh
+++ action.sh
@@ -2,7 +2,7 @@
 
 set -eux
 curl \
-    --data "${INPUTS_DATA}" \
+    --data "{\"event_type\": \"${INPUTS_EVENT_TYPE}\", \"client_payload\": ${INPUTS_CLIENT_PAYLOAD}}" \
     --header "Accept: application/vnd.github.v3+json" \
     --request POST \
     "https://${GITHUB_REPOSITORY_OWNER}:${INPUTS_TOKEN}@api.github.com/repos/${INPUTS_REPOSITORY}/dispatches"
