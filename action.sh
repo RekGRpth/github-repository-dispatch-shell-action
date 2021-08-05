817ae39af6287f7ba8774bb9e4c40a2e979e4687
RekGRpth <rekgrpth@gmail.com>
2021-08-05 13:34:01 +0500

    hub

--- action.sh
+++ action.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
 set -eux
-curl \
-    --data "{\"event_type\": \"${INPUTS_EVENT_TYPE}\", \"client_payload\": ${INPUTS_CLIENT_PAYLOAD}}" \
-    --header "Accept: application/vnd.github.v3+json" \
-    --request POST \
-    "https://${GITHUB_REPOSITORY_OWNER}:${INPUTS_TOKEN}@api.github.com/repos/${INPUTS_REPOSITORY}/dispatches"
+echo "{\"event_type\": \"${INPUTS_EVENT_TYPE}\", \"client_payload\": ${INPUTS_CLIENT_PAYLOAD}}" | hub api "repos/${INPUTS_REPOSITORY}/dispatches" --input -
