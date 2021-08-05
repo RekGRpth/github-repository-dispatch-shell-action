2a59b11e2d619eed5dbd9a240eb9538091a03310
RekGRpth <rekgrpth@gmail.com>
2021-08-05 14:22:01 +0500

    debug

--- action.sh
+++ action.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 set -eux
+env | sort
 echo "{\"event_type\": \"${INPUTS_EVENT_TYPE}\", \"client_payload\": ${INPUTS_CLIENT_PAYLOAD}}" | hub api "repos/${INPUTS_REPOSITORY}/dispatches" --input -
