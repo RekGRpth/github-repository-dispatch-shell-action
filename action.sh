e2f488167759d4f732ee47ec4fe861c3ad94b935
RekGRpth <rekgrpth@gmail.com>
2021-08-05 16:46:05 +0500

    fix

--- action.sh
+++ action.sh
@@ -2,4 +2,4 @@
 
 set -eux
 env | sort
-echo "{\"event_type\": \"${INPUTS_EVENT_TYPE:-${GITHUB_REF##*/}}\", \"client_payload\": ${INPUTS_CLIENT_PAYLOAD:-'{}'}}" | hub api "repos/${INPUTS_REPOSITORY:-${GITHUB_REPOSITORY}}/dispatches" --input -
+echo "{\"event_type\": \"${INPUTS_EVENT_TYPE:-${GITHUB_REF##*/}}\", \"client_payload\": {${INPUTS_CLIENT_PAYLOAD}}}" | hub api "repos/${INPUTS_REPOSITORY:-${GITHUB_REPOSITORY}}/dispatches" --input -
