0a2899339459b050c2c626f7ef14e3b863064e80
RekGRpth <rekgrpth@gmail.com>
2021-08-05 20:42:23 +0500

    up

--- action.sh
+++ action.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 set -eux
-env | sort
 echo "{\"event_type\": \"${INPUTS_EVENT_TYPE:-${GITHUB_REF##*/}}\", \"client_payload\": {${INPUTS_CLIENT_PAYLOAD}}}" | hub api "repos/${INPUTS_REPOSITORY:-${GITHUB_REPOSITORY}}/dispatches" --input -
