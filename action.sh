045aae3a733ee5e978936e29f98f1383be0fe3aa
RekGRpth <rekgrpth@gmail.com>
2023-10-06 08:37:00 +0500

    hub -> gh

--- action.sh
+++ action.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 set -eux
-echo "{\"event_type\":\"${INPUTS_EVENT_TYPE:-${GITHUB_REF##*/}}\",\"client_payload\":${INPUTS_CLIENT_PAYLOAD}}" | hub api "repos/${INPUTS_REPOSITORY:-${GITHUB_REPOSITORY}}/dispatches" --input -
+echo "{\"event_type\":\"${INPUTS_EVENT_TYPE:-${GITHUB_REF##*/}}\",\"client_payload\":${INPUTS_CLIENT_PAYLOAD}}" | gh api "repos/${INPUTS_REPOSITORY:-${GITHUB_REPOSITORY}}/dispatches" --input -
